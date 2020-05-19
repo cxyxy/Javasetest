d4616189e15f1bafe9a748d5b38de582e566c1d3
cxyxy <1328256298@qq.com>
2020-05-19 10:52:00 +0800

    instant，localdate,时间调整器

---
+++ src/common/java_main/WorkDayTemporalAdjuster.java
@@ -0,0 +1,14 @@
+package common.java_main;
+
+import java.time.LocalDate;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+public class WorkDayTemporalAdjuster implements TemporalAdjuster {
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+
+
+        return null;
+    }
+}
